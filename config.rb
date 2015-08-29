4101bf7292b68b6e708c310c6899e1ef9d5b4db1
August W. Miller <hello@gusmiller.com>
2015-08-29 15:43:07 -0700

    Correct `http_prefix`

--- config.rb
+++ config.rb
@@ -68,7 +68,7 @@ configure :build do
   # activate :relative_assets
 
   # Or use a different image path
-  set :http_prefix, "/InvestMap"
+  set :http_prefix, "/InvestMap/"
 end
 
 activate :directory_indexes
